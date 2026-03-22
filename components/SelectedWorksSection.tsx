16063a39d25ebe3b5de18a33444f6bff96253a60
Patrice DAGBE <patriko.dagbe@epitech.eu>
2026-03-22 14:56:52 +0100

    feat: done with home page

--- components/SelectedWorksSection.tsx
+++ components/SelectedWorksSection.tsx
@@ -12,10 +12,10 @@ const SelectedWorksSection = () => {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <ProjectCard title="MoMo" category="Redesign of MoMo UI" image="/ressourses/momo.png" />
+            <ProjectCard title="MoMo" category="Reconstruction of MoMo’s journey" image="/ressourses/momo.png" />
             <ProjectCard title="Medi Reminder" category="Mobile App" image="/ressourses/medi_reminder.png" />
-            <ProjectCard title="Medi Reminder" category="Mobile App" image="/ressourses/medi_reminder.png" />
-            <ProjectCard title="Medi Reminder" category="Mobile App" image="/ressourses/medi_reminder.png" />
+            <ProjectCard title="Secure Box from Secure Destination " category="Born for locker" image="/ressourses/secure_box.png" />
+            <ProjectCard title="Beyond The Gate" category="Immersive Experiences  " image="/ressourses/beyond_the_gate.png" />
           </div>
 
           <div className="pt-10 pb-2">
